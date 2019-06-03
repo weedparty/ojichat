2a97dfc595e7622448ff2f0a5fb250456cb15593
lm9 <depkey@me.com>
2019-06-03 19:27:13 +0900

    おじちゃん復活～ :kissing_closed_eyes:

--- ojichat.go
+++ ojichat.go
@@ -4,39 +4,87 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
-	"github.com/docopt/docopt-go"
+	"github.com/bwmarrin/discordgo"
 	"github.com/greymd/ojichat/generator"
+	"github.com/joho/godotenv"
 )
 
-var appVersion = `Ojisan Nanchatte (ojichat) command version 0.2.0
-Copyright (c) 2019 Yamada, Yasuhiro
-Released under the MIT License.
-https://github.com/greymd/ojichat`
-
-var usage = `Usage:
-  ojichat [options] [<name>]
-
-Options:
-  -h, --help      ヘルプを表示.
-  -V, --version   バージョンを表示.
-  -e <number>     絵文字/顔文字の最大連続数 [default: 4].
-  -p <level>      句読点挿入頻度レベル [min:0, max:3] [default: 0].`
-
-// TODO: --type おじさんタイプ (絵文字乱用, 顔文字乱用, 句読点, 若作り)
+var (
+	Token     = ""
+	OjichanID = ""
+	Command   = ""
+	active    = false
+	stopBot   = make(chan bool)
+)
 
 func main() {
-	parser := &docopt.Parser{
-		OptionsFirst: true,
+
+	godotenv.Load()
+
+	Token = os.Getenv("TOKEN")
+	OjichanID = os.Getenv("OJICHATID")
+	Command = os.Getenv("COMMAND")
+
+	discord, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println(err)
 	}
-	args, _ := parser.ParseArgs(usage, nil, appVersion)
-	config := generator.Config{}
-	args.Bind(&config)
+
+	discord.AddHandler(onMessageCreate)
+	err = discord.Open()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("おじちゃんは聞き耳を立てています...")
+	<-stopBot
+	return
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	c, err := s.State.Channel(m.ChannelID)
+
+	if err != nil {
+		return
+	}
+
+	if m.Author.ID == OjichanID {
+		return
+	}
+
+	if strings.Contains(m.Content, Command) || strings.Contains(m.Content, "死ね") {
+		active = !active
+		if !active {
+			s.ChannelMessageSend(c.ID, "じゃあね")
+			return
+		}
+	}
+
+	if active {
+		replyOjichat(s, m.Message, c.ID)
+	}
+}
+
+func replyOjichat(s *discordgo.Session, m *discordgo.Message, cid string) {
+	member, err := s.GuildMember(m.GuildID, m.Author.ID)
+	if err != nil {
+		return
+	}
+
+	name := member.Nick
+	if name == "" {
+		name = m.Author.Username
+	}
+
+	config := generator.Config{TargetName: name}
 
 	result, err := generator.Start(config)
 	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+		return
 	}
-	fmt.Printf("%s\n", result)
+
+	s.ChannelMessageSend(cid, result)
+	log.Println(result)
 }
